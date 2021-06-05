b9c1d310b199932fa8ac7c2bacd2ec37da4adcd7
hpdipto <haris.dipto@gmail.com>
2021-06-05 10:44:21 +0600

    login added

--- schema/schema.js
+++ schema/schema.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const graphql = require("graphql");
 const {
     GraphQLID,
@@ -13,7 +14,7 @@ const {
 const User = require("../models/user.models");
 
 // Create Type
-const UserType = new graphql.GraphQLObjectType({
+const UserType = new GraphQLObjectType({
     name: "User",
     description: "Documentation for User",
     fields: () => ({
@@ -70,6 +71,37 @@ const Mutation = new GraphQLObjectType({
                 user.save();
 
                 return user;
+            },
+        },
+        login: {
+            type: GraphQLString,
+            args: {
+                email: {
+                    type: new GraphQLNonNull(GraphQLString),
+                },
+                password: {
+                    type: new GraphQLNonNull(GraphQLString),
+                },
+            },
+            async resolve(parent, args) {
+                let { email, password } = args;
+                let user = await User.findOne({ email: email });
+                if (!user) {
+                    throw new Error("No user found with this email");
+                }
+                let loginSuccess = await bcrypt.compare(password, user.password);
+                if (loginSuccess) {
+                    const token = jwt.sign(
+                        {
+                            user: user.id,
+                        },
+                        "secRet",
+                        {
+                            expiresIn: "7d",
+                        }
+                    );
+                    return token;
+                } else throw new Error("Passowrd didn't match");
             },
         },
     },
